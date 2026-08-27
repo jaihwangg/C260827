5fb9f83ea6ec8d4b9c20f6dc88516e9268fd3191
jaihwang <jaihwangg@gmail.com>
2026-08-27 10:50:59 +0900

    몬스터 3종 클래스

---
+++ Goblin.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "Monster.h"
+
+class AGoblin
+{
+public:
+	AGoblin()
+	{ }
+	~AGoblin()
+	{ }
+
+};
+
---
+++ WildBoar.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "Character.h"
+
+class AWildBoar : public ACharacter
+{
+public:
+	AWildBoar();
+	~AWildBoar();
+
+
+};
+
